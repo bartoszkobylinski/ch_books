754d46202006560e7a15989013f61bb280bc4fc4
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2021-05-10 20:36:04 +0200

    MODYFING SETTINGS FILE FOR UPLOAD IMAGES
    - modyfing settings for upload images
    - modyfing index template for uploading images

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from books.views import book_insert_view, IndexView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('insert_book', book_insert_view),
     path('', IndexView.as_view())
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
