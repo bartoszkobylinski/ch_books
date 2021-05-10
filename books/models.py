1f5e030f6d1ff87733650b598317a9087e15a245
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2021-05-10 11:34:58 +0200

    ADDING TWO MODELS
    - adding clubhouse room and book model
    
    - register models to admin register

--- books/models.py
+++ books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class ClubHouseRoom(models.Model):
+    room_name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f"Clubhouse {self.room_name}"
+
+
+class Book(models.Model):
+    club_house_room = models.ForeignKey(ClubHouseRoom, on_delete=models.CASCADE)
+    author = models.CharField(max_length=150)
+    title = models.CharField(max_length=150)
+    cover = models.FileField(upload_to=covers/)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
