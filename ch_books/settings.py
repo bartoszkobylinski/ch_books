1a41e78d504bb59077deb4a2e3a6cc351b04c894
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2021-05-15 20:12:03 +0200

    ADDING HEROKU PACKAGE
    - adding heroku package for easier deploying

--- ch_books/settings.py
+++ ch_books/settings.py
@@ -141,3 +141,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
