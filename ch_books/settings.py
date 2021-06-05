0eb8796a2578d4fb57ce7afadd1e756549857b5e
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2021-06-05 16:35:40 +0200

    updating settings.py

--- ch_books/settings.py
+++ ch_books/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['still-fjord-39299.herokuapp.com', '127.0.0.1']
-
+# ALLOWED_HOSTS = ['still-fjord-39299.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
