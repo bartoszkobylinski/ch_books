37b160d98d81a18495434906b4f750abe7549b4d
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2021-06-05 22:43:41 +0200

    update settings.py

--- ch_books/settings.py
+++ ch_books/settings.py
@@ -134,10 +134,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'books/static/'),
-)
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
