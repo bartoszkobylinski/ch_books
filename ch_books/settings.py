06dd5654e1ccd396640e9e67ebf176c696e5e89d
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2021-06-05 17:04:59 +0200

    updating settings.py

--- ch_books/settings.py
+++ ch_books/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'books',
 ]
 
