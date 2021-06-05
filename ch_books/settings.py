e34cf3d7994115cd6f30dddbb21bea7e5ad7ac12
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2021-06-05 17:50:58 +0200

    updating settings.py

--- ch_books/settings.py
+++ ch_books/settings.py
@@ -49,13 +49,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ch_books.urls'
@@ -134,11 +134,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-'''
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'books/static'),
 )
-'''
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
