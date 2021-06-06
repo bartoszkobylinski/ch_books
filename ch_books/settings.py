27d62c48ad937ca5cb50434d1fcab5275bc596d2
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2021-06-06 23:35:05 +0200

    ADDING AWS CREDENTIALS AND AWS DATA
    - adding aws boto library
    - adding aws settings

--- ch_books/settings.py
+++ ch_books/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
     'books',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -147,3 +148,17 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AWS SETTINGS
+
+AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
+
+AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
+
+AWS_STORAGE_BUCKET_NAME = env.str('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
